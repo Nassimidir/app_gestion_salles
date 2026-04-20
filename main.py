0eeb38f9efd3827810bf1f8d0f60230cffb5d614
Nassim Idir <nassimidir401@gmail.com>
2026-04-19 21:31:20 -0400

    Ajout du test de connexion a la base de donnees

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from data.dao_salle import DataSalle
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data_salle = DataSalle()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+connexion = data_salle.get_connection()
+if connexion.is_connected():
+    print("Connexion a la base de donnees reussie")
+    connexion.close()
+else:
+    print("Echec de la connexion")
